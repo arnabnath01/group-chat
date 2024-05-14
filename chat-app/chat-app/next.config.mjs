10d88035f21ac3286d6b8c9bf05487f4f08caa94
Arnab Nath <arnabnath123456789@gmail.com>
2024-05-14 21:33:16 +0530

    msg reading frm supabse and listing chats

--- chat-app/chat-app/next.config.mjs
+++ chat-app/chat-app/next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                hostname: 'avatars.githubusercontent.com',
-                protocol: 'https',
-            }
-        ]
-       
-    }
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https",
+      },
+      {
+        hostname: "avatars.githubusercontent.com",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- chat-app/chat-app/src/components/ChatMessages.tsx
+++ chat-app/chat-app/src/components/ChatMessages.tsx
@@ -6,59 +6,35 @@ import { supabaseServer } from '@/lib/supabase/server';
 
 
 export default async function ChatMessages(){
-//   const msgData = supabaseServer();
+  // const msgData = supabaseServer();
 
-//   const { data } = await msgData
-//     .from("messages")
-//     .select("*,users(*)")
-//     .order("created_at", { ascending: false });
+  // const { data } = await msgData
+  //   .from("messages")
+  //   .select("*,users(*)")
+  //   .order("created_at", { ascending: false });
 
-//   console.log(data)
+  // console.log(data)
 
-// const supabase = supabaseServer()
-// const {data} = await supabase.from("messages").select("*,users(*)")
-// console.log(data)
+const supabase =await supabaseServer()
 
-  const sampleData = [
-    {
-      created_at: "2022-01-01T00:00:00Z",
-      id: "1",
-      is_edit: false,
-      send_by: "user1",
-      text: "Hello, this is a message",
-      users: {
-        //   avatar_url: "https://example.com/avatar1.png",
-        avatar_url: "",
-        created_at: "2022-01-01T00:00:00Z",
-        display_name: "User One",
-        id: "user1",
-      },
-    },
-    {
-      created_at: "2022-01-02T00:00:00Z",
-      id: "2",
-      is_edit: false,
-      send_by: "user2",
-      text: "Hello, this is another message",
-      users: {
-        avatar_url: "",
-        created_at: "2022-01-02T00:00:00Z",
-        display_name: "User Two",
-        id: "user2",
-      },
-    },
-  ];
+// console.log(supabase)
+const {data} = await supabase.from("messages").select("*,users(*)").order("created_at", { ascending: false });
+
+console.log(data?.length)
+
+// console.log(data[0])
+
 
   return (
     // suspense is used to handle async data
-    <Suspense fallback="loading..">
+    <Suspense fallback={"loading.."}>
 
       {/* lists all the messages  */}
       <ListMessages />
 
       {/* data can be empty */}
-      {/* <InitMessages messages={data || []} /> */}
-      <InitMessages messages={sampleData || []} />
+      <InitMessages messages={data || []} />
+      
     </Suspense>
   );
 }
